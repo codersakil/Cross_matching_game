131041cdaadde3eb1cd5e5da100b7b4f873b34ea
Sakil <sakilexpart@gmail.com>
2020-08-11 11:44:22 +0530

    fixed bug and create a player score board

--- Cross_matching_game.py
+++ Cross_matching_game.py
@@ -9,10 +9,14 @@ Label(text='Welcome',font='comicsams 20',fg='red',bg='yellow').pack(pady=5,fill=
 player_frame=Frame(root,bd=10, relief=SUNKEN)
 player_frame.pack(pady=10)
 Label(player_frame,text='Choose the playing mode',font='comicsams 15',fg='green').grid(row=1,column=1,columnspan=2)
+
 db=Frame(root,bd=5)
 db.pack()
-scr1=Label()
+scr1,scr2=0,0
+plscr1=Label(db,text=f'Player 1 score:\n{scr1}')
+plscr2=Label(db,text=f'Player 2 score:\n{scr2}')
 play=Label(db,text=f'It is time of player {player}',bg='violet')
+
 mode=0
 def single():
 	global mode
@@ -26,7 +30,7 @@ def double():
 	new()
 	p2.config(state='disable',bg='green')
 	p1.config(state=NORMAL,bg='grey')
-	play.pack(pady=5)
+	play.grid(row=2,column=1,columnspan=2,pady=5)
 
 p1=Button(player_frame,text='Single',command=single)
 p1.grid(row=2,column=1,pady=50)
@@ -100,21 +104,28 @@ def new():
 	l1=[b1,b2,b3,b4,b5,b6,b7,b8,b9]
 	l2=[]
 	l3=[]
-	f1.pack_forget()
 	f1.pack()
 	global player
 	if mode==2:
 		b_com.grid_forget()
 		player=random.randint(1,2)
+		
+		plscr1.grid(row=1,column=1,padx=20)
+		plscr2.grid(row=1,column=2,padx=20)
 		play.config(text=f'It is time of player {player}')
 	else:
+		plscr1.grid_forget()
+		plscr2.grid_forget()
+		play.grid_forget()
 		b_com.grid(row=4,column=1,columnspan=3,pady=50)
+		
 
 
 
 	
 def check(list):
 	match=[[b1,b2,b3],[b1,b4,b7],[b1,b5,b9],[b2,b5,b8],[b3,b6,b9],[b3,b5,b7],[b4,b5,b6],[b7,b8,b9]]
+	global scr1,scr2
 	for cl in match:
 		i=0
 		for item in cl:
@@ -126,10 +137,14 @@ def check(list):
 					tmsg.showinfo('Match result','Congratulatio,You won')
 				else:
 					tmsg.showinfo('Match result','Congratulation player1,You won')
+					scr1+=1
+					plscr1.config(text=f'Player 1 score:\n{scr1}')
 			else:
 				if mode==1:
 					tmsg.showinfo('Match result','Sorry,You loose')
 				else:
 					tmsg.showinfo('Match result','Congratulation player2,You won')
+					scr2+=1
+					plscr2.config(text=f'Player 2 score:\n{scr2}')
 
 root.mainloop()
